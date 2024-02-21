b8e9716286cd536608fe7231077e8b364dea53f0
Digvijay8116 <100955521+Digvijay8116@users.noreply.github.com>
2024-02-21 23:26:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://e-comm-front-two.vercel.app", //just give the local host:3000 not the complete path
+    origin: "*", 
     method: ["GET", "POST", "PUT", "DELETE"],
   },
 });
