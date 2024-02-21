98b8ca08aac227e7416aa1e55357eca83fb18973
Digvijay8116 <100955521+Digvijay8116@users.noreply.github.com>
2024-02-21 23:53:09 +0530

    Update Backend_URI.js

--- frontend/src/components/Backend_URI.js
+++ frontend/src/components/Backend_URI.js
@@ -1,4 +1,4 @@
 // const BACKEND_URI = "http://localhost:9002";
-const BACKEND_URI = "https://e-comm-api2.vercel.app";
+const BACKEND_URI = "https://e-comm-back.vercel.app";
 
 export { BACKEND_URI };
