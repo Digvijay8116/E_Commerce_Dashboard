c54f933266caaed09aaf45ee41008ea064ad6170
Digvijay8116 <100955521+Digvijay8116@users.noreply.github.com>
2024-02-21 23:10:56 +0530

    Update Backend_URI.js

--- frontend/src/components/Backend_URI.js
+++ frontend/src/components/Backend_URI.js
@@ -1,4 +1,4 @@
 // const BACKEND_URI = "http://localhost:9002";
-const BACKEND_URI = "https://digvijay-m1back.mobiloitte.io";
+const BACKEND_URI = "https://e-comm-api2.vercel.app";
 
 export { BACKEND_URI };
